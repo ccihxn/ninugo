16f5714286774a42db69f198f25110b32e9690dc
전지환 <wjswlghks7@ks.ac.kr>
2023-09-05 04:00:14 +0900

    commt

--- src/main/java/com/ks/ninugo/dto/UserDTO.java
+++ src/main/java/com/ks/ninugo/dto/UserDTO.java
@@ -1,15 +1,21 @@
 package com.ks.ninugo.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
 @Data
 public class UserDTO {
+    @NonNull
     int id;
+    @NonNull
     String nickname;
+    @NonNull
     String loginId;
+    @NonNull
     String password;
+    @NonNull
     String email;
     int userState;
     String role;
