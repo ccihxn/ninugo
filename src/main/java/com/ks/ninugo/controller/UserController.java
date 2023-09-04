bd7d4ced07ba4e1756ca6b72045a4297e9880249
pwrwpw <dlgustjr9912@gmail.com>
2023-09-05 05:41:08 +0900

    controller 목업 생성 및 테스트

--- src/main/java/com/ks/ninugo/controller/UserController.java
+++ src/main/java/com/ks/ninugo/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.ks.ninugo.controller;
 
+import com.ks.ninugo.dto.UserDTO;
+import com.ks.ninugo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+    @GetMapping("/get")
+    public void getUser() {
+        userService.findUserById(6);
+    }
+    @PostMapping("/create")
+    public void createUser() {
+        userService.insertUser(new UserDTO("test", "test", "test", "test", "user"));
+    }
 }
