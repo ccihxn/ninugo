ee441e88efe4d5e7d1802f8f5b590e2e14ea5e0b
pwrwpw <dlgustjr9912@gmail.com>
2023-09-05 05:41:43 +0900

    service 매퍼 생성자 생성

--- src/main/java/com/ks/ninugo/service/UserService.java
+++ src/main/java/com/ks/ninugo/service/UserService.java
@@ -3,22 +3,27 @@ package com.ks.ninugo.service;
 import com.ks.ninugo.dto.UpdateUserDTO;
 import com.ks.ninugo.dto.UserDTO;
 import com.ks.ninugo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    UserMapper userMapper;
-    private void insertUser(UserDTO userDTO) {
-        userMapper.insertUser(userDTO);
+    private final UserMapper userMapper;
+    @Autowired
+    public UserService(UserMapper userMapper) {
+        this.userMapper = userMapper;
     }
-    private UserDTO findUserById(int userId) {
+    public void insertUser(UserDTO userDTO) {
+        userMapper.insertUser(userDTO);
+        System.out.println(userDTO);
+    }
+    public UserDTO findUserById(int userId) {
         return userMapper.findUserById(userId);
     }
-    private void deleteUserById(int userId) {
+    public void deleteUserById(int userId) {
         userMapper.deleteUserById(userId);
     }
-    private void updateUser(UpdateUserDTO updateUserDTO) {
+    public void updateUser(UpdateUserDTO updateUserDTO) {
         userMapper.updateUser(updateUserDTO);
     }
 }
