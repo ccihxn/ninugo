e0ac603fed8fee9d43bc1936274cd6c1ddac6ea9
전지환 <wjswlghks7@ks.ac.kr>
2023-09-05 00:59:02 +0900

    first commit

---
+++ src/test/java/com/ks/ninugo/NinugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.ninugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
