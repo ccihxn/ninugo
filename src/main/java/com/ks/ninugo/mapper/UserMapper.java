3a0aca575ac74cec903455ec0e4fc4f3bf4fdd8a
pwrwpw <dlgustjr9912@gmail.com>
2023-09-05 05:41:23 +0900

    Mapper 인터페이스 수정

--- src/main/java/com/ks/ninugo/mapper/UserMapper.java
+++ src/main/java/com/ks/ninugo/mapper/UserMapper.java
@@ -2,10 +2,14 @@ package com.ks.ninugo.mapper;
 
 import com.ks.ninugo.dto.UpdateUserDTO;
 import com.ks.ninugo.dto.UserDTO;
-//import org.apache.mybatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
 public interface UserMapper {
-    public void insertUser(UserDTO userDTO);
-    public UserDTO findUserById(int userId);
-    public void deleteUserById(int userId);
-    public void updateUser(UpdateUserDTO updateUserDTO);
+    void insertUser(UserDTO userDTO);
+    UserDTO findUserById(int userId);
+    void deleteUserById(int userId);
+    void updateUser(UpdateUserDTO updateUserDTO);
 }
