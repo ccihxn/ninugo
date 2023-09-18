f11c6cd21aeaff64dddbf70e44735d078af32461
pwrwpw <dlgustjr9912@gmail.com>
2023-09-18 09:52:14 +0900

    security 생성

--- src/main/java/com/ks/ninugo/configure/security/SecurityConfig.java
+++ src/main/java/com/ks/ninugo/configure/security/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.ks.ninugo.configure.security;
 
+import com.ks.ninugo.configure.security.provider.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                // 특정 URL에 대한 권한 설정.
+                .authorizeHttpRequests((authorizeRequests) -> {
+                    authorizeRequests.requestMatchers("/user/**").authenticated();
+
+                    authorizeRequests.requestMatchers("/admin/**")
+                            .hasRole("ADMIN");
+
+                    authorizeRequests.anyRequest().permitAll();
+                })
+
+                .formLogin((formLogin) -> {
+                    /* 권한이 필요한 요청은 해당 url로 리다이렉트 */
+                    formLogin.loginPage("/login");
+                })
+                .build();
+    }
 }
--- src/main/java/com/ks/ninugo/configure/security/provider/JwtTokenProvider.java
+++ src/main/java/com/ks/ninugo/configure/security/provider/JwtTokenProvider.java
@@ -1,4 +1,7 @@
 package com.ks.ninugo.configure.security.provider;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtTokenProvider {
 }
